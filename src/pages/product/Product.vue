5848c3f713fca6e2bb0ab4bd896b8a1af655059b
dtuanv <dtuanlieb@gmail.com>
2022-12-12 21:53:43 +0100

    use watch option to trigger function

--- src/pages/product/Product.vue
+++ src/pages/product/Product.vue
@@ -1,63 +1,71 @@
 <template>
   <q-page :class="$q.screen.lt.sm ? 'q-pa-md' : 'q-pa-sm'">
-    <div>Hi
-      <q-select
-        label="Tất cả thương hiệu"
-        transition-show="flip-up"
-        transition-hide="flip-down"
-        filled
-        v-model="marktSelected"
-        :options="marktOptions"
-        style="width: 250px"
-      />
+
+    <div class="" >
+
+        <div class="flex flex-center q-mt-lg" >
+          <q-input  style="width:30rem" v-model="findProduct" :rules="findProductValidate" label="Nhập tên sản phẩm" rounded outlined >
+            <template v-slot:append>
+              <q-btn flat icon="search" @click="findProductByName"/>
+        </template>
+      </q-input>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-4">
+        <q-select label="Tất cả thương hiệu" transition-show="flip-up" transition-hide="flip-down" filled
+          v-model="marktSelected" :options="marktOptions" style="width: 250px" />
+      </div>
+      <div class="col-2"></div>
+      <div class="col-4">
+        <q-select label="Sắp Xếp" transition-show="flip-up" transition-hide="flip-down" filled v-model="filterSelected"
+          :options="filterOptions" style="width: 250px" />
+      </div>
 
     </div>
+
     <div>
-      <div class="row" >
+      <div class="row">
         <div class="row" :style="$q.screen.lt.sm ? '' : 'width:92%'">
-          <div
-            :style="$q.screen.lt.sm ? 'width:100%' : 'width: 19rem;'"
-            v-for="product in productsCategory.filter(p =>{
-             return p.status == true
-            })"
-            :key="product.id"
-          >
-            <productBox :product="product" ></productBox>
+          <div :style="$q.screen.lt.sm ? 'width:100%' : 'width: 19rem;'" v-for="product in productsCategory.filter(p => {
+            return p.status == true
+          })" :key="product.id">
+            <productBox :product="product"></productBox>
           </div>
         </div>
         <!-- <div > -->
 
-        <div :style="$q.screen.lt.sm ? '':'max-width:8%'" :class="$q.screen.lt.sm ? 'row':''">
-          <div class="q-pt-lg">
-            <q-btn>
-            <q-avatar><img src="/img/icon/zalo.png" alt="" /></q-avatar>
-          </q-btn>
+        <div :style="$q.screen.lt.sm ? '' : 'max-width:8%'" :class="$q.screen.lt.sm ? 'row' : ''">
+          <div class="q-pt-lg ">
+            <q-btn class="btn" flat>
+              <q-avatar><img src="/img/icon/zalo.png" alt="" /></q-avatar>
+            </q-btn>
           </div>
           <div class="q-pt-sm">
-            <q-btn>
-            <q-avatar><img src="/img/icon/facebook.png" alt="" /></q-avatar>
-          </q-btn>
+            <q-btn class="btn" flat>
+              <q-avatar><img src="/img/icon/facebook.png" alt="" /></q-avatar>
+            </q-btn>
           </div>
 
-          <div class="q-pt-sm">
-            <q-btn>
-            <q-avatar><img src="/img/icon/shopee.png" alt="" /></q-avatar>
-          </q-btn>
+          <div class="q-pt-sm btnS">
+            <q-btn class="btn" flat>
+              <q-avatar><img src="/img/icon/shopee.png" alt="" /></q-avatar>
+            </q-btn>
           </div>
           <div class="q-pt-sm">
-            <q-btn>
-            <q-avatar><img src="/img/icon/lazada.png" alt="" /></q-avatar>
-          </q-btn>
+            <q-btn class="btn" flat>
+              <q-avatar><img src="/img/icon/lazada.png" alt="" /></q-avatar>
+            </q-btn>
           </div>
           <div class="q-pt-sm">
-            <q-btn>
-            <q-avatar><img src="/img/icon/instagram.png" alt="" /></q-avatar>
-          </q-btn>
+            <q-btn class="btn" flat>
+              <q-avatar><img src="/img/icon/instagram.png" alt="" /></q-avatar>
+            </q-btn>
           </div>
           <div class="q-pt-sm">
-            <q-btn>
-            <q-avatar><img src="/img/icon/sendo.png" alt="" /></q-avatar>
-          </q-btn>
+            <q-btn class="btn" flat>
+              <q-avatar><img src="/img/icon/sendo.png" alt="" /></q-avatar>
+            </q-btn>
           </div>
 
         </div>
@@ -99,12 +107,22 @@ import Product from "/src/apis/Product.js";
 import { WebApi } from "/src/apis/WebApi";
 
 const marktOptions = [
-  {label:'Đức', name:'duc'},
-  {label:'Ý', name:'y'},
+  { label: 'Đức', name: 'duc' },
+  { label: 'Ý', name: 'y' },
 ]
+
+
+const filterOptions = [
+  { label: 'Giá từ thấp đến cao', name: 'lowToHight' },
+  { label: 'Giá từ cao đến thấp', name: 'hightToLow' },
+  { label: 'Hot Sale', name: 'hotSale' },
+]
+
 
 const productsCategory = ref([]);
 const marktSelected = ref();
+const filterSelected = ref();
+const findProduct = ref('');
 export default {
   components: { productBox },
   setup() {
@@ -130,7 +148,7 @@ export default {
     const products = [
       {
         id: 0,
-        status:true,
+        status: true,
         imageUrl: "goidau.png",
         imageUrl2: "dauxa.png",
         imageUrl3: "nau.png",
@@ -138,91 +156,91 @@ export default {
         name: "Dầu gội Yunsey dành cho nam For Men 380ml",
         price: "130000",
         discount: 30,
-        subtitle:"BỘ PHỤC HỒI TÓC HƯ TỔN 3 BƯỚC MILBON DEESSE'S MẪU 2022 (600ML X 3): ",
-        body:'subtitle.png',
-        body1:"123 Thường: cho tóc hư tổn có chất tóc sợi mỏng, mảnh",
-        body2:"123+: cho tóc hư tổn có chất tóc sợi trung bình",
-        body3:"123X: cho tóc hư tổn có chất tóc sợi to, dày",
-        body4:"cho 2 tóc hư tổn có chất tóc sợi trung bình",
+        subtitle: "BỘ PHỤC HỒI TÓC HƯ TỔN 3 BƯỚC MILBON DEESSE'S MẪU 2022 (600ML X 3): ",
+        body: 'subtitle.png',
+        body1: "123 Thường: cho tóc hư tổn có chất tóc sợi mỏng, mảnh",
+        body2: "123+: cho tóc hư tổn có chất tóc sợi trung bình",
+        body3: "123X: cho tóc hư tổn có chất tóc sợi to, dày",
+        body4: "cho 2 tóc hư tổn có chất tóc sợi trung bình",
 
 
-        description:"description.png",
-        category:'hair',
+        description: "description.png",
+        category: 'hair',
       },
       {
         id: 1,
-        status:true,
-        subtitle:"BỘ PHỤC HỒI TÓC HƯ TỔN 3 BƯỚC MILBON DEESSE'S MẪU 2022 (600ML X 3): ",
+        status: true,
+        subtitle: "BỘ PHỤC HỒI TÓC HƯ TỔN 3 BƯỚC MILBON DEESSE'S MẪU 2022 (600ML X 3): ",
 
         imageUrl: "dauxa.png",
         name: "Dầu gội xả siêu mượt phục hồi và ngăn gãy rụng tóc Edocean Collagen Keratin 500mlx2",
         price: "130000",
-        discount: 30,
+        discount: 50,
         description: "",
       },
       {
         id: 2,
-        status:true,
+        status: true,
 
         imageUrl: "goidau.png",
         name: "Dau Goi Dau 3 Dau Goi Dau 3  Dầu gội xả siêu mượt phục hồi và ngăn gãy rụng tóc Edocean Collagen Keratin 500mlx2 ",
         price: "130000",
         discount: 20,
         description: "Dau Goi Dau De 3",
-        category:'hair',
+        category: 'hair',
 
       },
       {
         id: 3,
-        status:true,
+        status: true,
 
         imageUrl: "goidau.png",
         name: "Dau Goi Dau 3 Keratin thủy phân ORCHID_OIL KLERAL phục hồi cho mái tóc hư tổn, khô xơ, giòn, gẫy, xốp 10mlx10",
         price: "130000",
-        discount: 30,
+        discount: 25,
         description: "Dau Goi Dau De 3",
-        category:'hair',
+        category: 'hair',
 
       },
       {
         id: 4,
-        status:false,
+        status: false,
 
         imageUrl: "goidau.png",
         name: "Dau Goi Dau 4 F Dau Goi Dau 3 Keratin thủy phân ORCHID_OIL KLERAL phục hồi cho mái tóc hư tổn, khô xơ, giòn, gẫy, xốp 10mlx10",
         price: "130000",
         discount: 30,
         description: "Dau Goi Dau De 3",
-        category:'whitening',
+        category: 'whitening',
 
       },
       {
         id: 5,
-        status:true,
+        status: true,
 
         imageUrl: "nau.png",
         name: "TINH DẦU DƯỠNG TÓC KLERAL HUILE D’ARGAN CONDITIONING OIL 150ML",
         price: "130000",
         discount: 30,
         description: "Dau Goi Dau De 3",
-        category:'whitening',
+        category: 'whitening',
 
       },
       {
         id: 6,
-        status:true,
+        status: true,
 
         imageUrl: "nau.png",
         name: "TINH DẦU DƯỠNG TÓC KLERAL HUILE D’ARGAN CONDITIONING OIL 150ML",
         price: "130000",
         discount: 30,
         description: "Dau Goi Dau De 3",
-        category:'combo',
+        category: 'combo',
 
       },
       {
         id: 7,
-        status:true,
+        status: true,
 
         imageUrl: "nau.png",
         name: "TINH DẦU DƯỠNG TÓC KLERAL HUILE D’ARGAN CONDITIONING OIL 150ML",
@@ -241,17 +259,26 @@ export default {
       notice.value = response.data;
     });
     console.log("products.value", products);
-    if(route.params.category != undefined){
+    if (route.params.category != undefined) {
       productsCategory.value = products.filter(p => {
-      return p.category == route.params.category
-    })
-    }else{
+        return p.category == route.params.category
+      })
+    } else {
       productsCategory.value = products
     }
 
-    console.log("productsCategory.value", productsCategory.value);
-    console.log("route.params.category", route.params.category);
-const checkParam = ref(route.params.category)
+
+    const checkParam = ref(route.params.category)
+    function priceWithDiscount(price, discount) {
+      var priceInt = parseInt(price);
+      var rest = discount / 100;
+      return priceInt * (1 - rest);
+    }
+    function removeAccents(str) {
+  return str.normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, '')
+            .replace(/đ/g, 'd').replace(/Đ/g, 'D');
+}
     return {
       goToSushi,
       products,
@@ -261,13 +288,30 @@ const checkParam = ref(route.params.category)
       productsCategory,
       checkParam,
       marktSelected,
+      filterSelected,
       marktOptions,
+      filterOptions,
+      priceWithDiscount,
+      findProduct,
+      removeAccents,
+        findProductValidate: [
+        (val) =>
+          (val !== null &&
+            val !== "" &&
+            !!val ) ||
+          // val.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{5})$/g)) ||
+          "Vui lòng nhập đúng số đơn ạ!",
+        // (val) =>
+        //   (val.charAt(0) == 0 && !!val) || "Bitte geben Sie die richtige Telefonnummer mit 0 am Anfang ein",
+
+      ],
+
     };
   },
   mounted() {
     this.$store.dispatch("cache/getProduct");
   },
-  data() {},
+  data() { },
   methods: {
     scrollMeTo(refName) {
       var element = this.$refs[refName];
@@ -275,21 +319,64 @@ const checkParam = ref(route.params.category)
 
       window.scrollTo(0, top);
     },
+
+    findProductByName(){
+      const productsOrig = productsCategory.value
+
+      if(this.findProduct.length == 0){
+        productsCategory.value = productsOrig
+      }
+      var findName = this.findProduct
+      productsCategory.value = productsCategory.value.filter(p => {
+        console.log(" p.name.includes(this.findProduct) ",this.removeAccents(p.name).includes(this.removeAccents(findName)))
+        console.log("this.removeAccents(p.name) ",this.removeAccents(p.name)," findName   ",this.removeAccents(findName))
+
+        // return this.removeAccents(p.name).search(findName) !== -1
+        return this.removeAccents(p.name).toLowerCase().includes(this.removeAccents(findName).toLowerCase())
+        // return p.name.includes(findName)
+      })
+      console.log("findProduct ",this.findProduct)
+
+    }
+
+
+
   },
   components: { Detail, productBox },
   watch: {
     '$route'(to, from) {
-    const category = to.params.category
-    // this.AJAXRequest(id)
-    console.log("change product",category)
-        if(category != undefined){
-          productsCategory.value = this.products.filter(p => {
-      return p.category == category
-    })
-        }else{
-          productsCategory.value = this.products
-        }
-  }
+      const category = to.params.category
+      // this.AJAXRequest(id)
+      console.log("change product", category)
+      if (category != undefined) {
+        productsCategory.value = this.products.filter(p => {
+          return p.category == category
+        })
+      } else {
+        productsCategory.value = this.products
+      }
+    },
+    filterSelected() {
+      if (filterSelected.value.name == 'lowToHight') {
+        productsCategory.value = productsCategory.value.sort((a, b) => this.priceWithDiscount(a.price, a.discount) - this.priceWithDiscount(b.price, b.discount))
+      }
+      if (filterSelected.value.name == 'hightToLow') {
+        productsCategory.value = productsCategory.value.sort((a, b) => this.priceWithDiscount(b.price, b.discount) - this.priceWithDiscount(a.price, a.discount))
+      }
+
+      if (filterSelected.value.name == 'hotSale') {
+        productsCategory.value = productsCategory.value.sort((a, b) => b.discount - a.discount)
+      }
+
+    },
+
+    findProduct(newFind ,old){
+
+     if(newFind.length < old.length){
+
+      productsCategory.value = this.products
+     }
+    }
 
   }
 };
@@ -300,20 +387,28 @@ const checkParam = ref(route.params.category)
   max-width: 40em;
   display: inline-block;
 }
+
 .my-card {
   width: 100%;
   height: 100%;
   /* max-height: 450px;
     max-width : 300px */
 }
+
 .q-card__actions .q-btn {
   padding: 0px 0px;
 }
+
 .containerT {
   margin-inline: auto;
   width: (min(90%), 70.5em);
 }
+
 .q-card__section--vert {
   padding: 0rem 0.75rem 0px 0px;
+}
+
+.btn:hover {
+  background: lightgreen;
 }
 </style>
--- src/pages/shopping/Cart.vue
+++ src/pages/shopping/Cart.vue
@@ -6,24 +6,28 @@
       </div>
       <div v-for="(item, index) in items" :key="index" :item="item">
 
-        <div>{{ item.product.imageUrl }}</div>
 
 
 
-        <div class="justify-center flex row">
+        <div class="justify-center flex ">
           <div class="q-ma-sm row">
-            <div>{{ item.quantity }} x </div>
-            <div>
-              <q-avatar>
-                <img :src="'/img/' + item.product.imageUrl">
-              </q-avatar>
-            </div>
+            <q-chip>
+
+              <div style="margin-right: 24px">{{ item.quantity }} x </div>
+              <div>
+                <q-avatar>
+                  <img :src="'/img/' + item.product.imageUrl">
+                </q-avatar>
+              </div>
+
+              <div>
+                {{ item.product.name }}
+              </div>
+            </q-chip>
 
             <div>
-              {{ item.product.name }}
-            </div>
-            <div>
-              <q-btn @click.prevent="removeProductFromCart(item.product)" icon="delete" color="negative" dense flat></q-btn>
+              <q-btn @click.prevent="removeProductFromCart(item.product)" icon="delete" color="negative" dense
+                flat></q-btn>
 
             </div>
           </div>
@@ -41,28 +45,100 @@
         <q-btn @click="dialog_bill = true" class="" label="Check Out" style="color: goldenrod"></q-btn>
       </div>
     </div>
-    <q-dialog v-model="dialog_bill" class="">
-      <q-card class="my-card">
+
+      <q-card class="q-pa-md" v-if="dialog_bill">
         <q-card-section>
-          <div class="text-h4 flex justify-center">Delivery</div>
+          <div class="text-h4 flex justify-center" style="font-family: cursive;color: cadetblue;">Thông tin khách hàng
+          </div>
         </q-card-section>
         <q-separator />
 
         <q-card-actions vertical>
           <q-form @submit="itemCheckOut">
             <!-- Input Validation -->
-            <q-input v-model="user.name" class="col-4" label="Name" color="white" :rules="[
+            <q-input v-model="customer.name" class="col-4" label="Tên" color="white" :rules="[
               (val) =>
-                (!!val && val.length > 1) || 'Please write a correct name',
+                (!!val && val.length > 1) || 'Xin vui lòng nhập đúng tên ạ',
             ]"></q-input>
-            <q-input v-model="user.adresse" class="col-4" label="Adresse" color="white" :rules="adresseRules"></q-input>
-            <q-input v-model="user.mobil" class="col-4" label="Mobil" color="white" :rules="mobilRules"></q-input>
-            <q-input label="Note" v-model="user.note" autogrow />
-            <q-btn class="q-mt-lg float-right" label="Send" color="primary" type="submit" dense></q-btn>
+            <q-input v-model="customer.adresse" class="col-4" label="Địa chỉ" color="white"
+              :rules="adresseRules"></q-input>
+            <q-input v-model="customer.mobil" class="col-4" label="SĐT" color="white" :rules="mobilRules"></q-input>
+            <q-input label="Ghi chú" v-model="customer.note" autogrow />
+            <q-btn class="q-mt-lg float-right" label="Gửi" color="primary" type="submit" dense></q-btn>
           </q-form>
         </q-card-actions>
       </q-card>
-    </q-dialog>
+    <!-- Payment beginn -->
+
+      <q-card class="q-pa-md" v-if="dialog_payment">
+        <q-card-section>
+          <div class="text-h4 flex justify-center" style="font-family: cursive;color: cadetblue;">Hình Thức Thanh toán
+          </div>
+        </q-card-section>
+        <q-separator />
+
+        <q-card-actions vertical  >
+          <div v-if="expand_codPayment == false && expand_cardPayment == false">*Vui lòng chọn hình thức thanh toán</div>
+          <div class="row">
+            <q-item clickable style="width:50%" :style="expand_codPayment == true && expand_cardPayment == false ? 'background-color: bisque;' :'' " class="column" @click="expand_codPayment = true,expand_cardPayment = false" >
+              <div class=" flex flex-center">
+                <q-avatar>
+                  <img src="/img/icon/codPayment.png" alt="">
+                </q-avatar>
+              </div>
+
+              <div class="q-mt-xs flex flex-center">
+                Trả tiền khi nhận hàng (COD)
+              </div>
+            </q-item>
+            <q-item clickable style="width:50%" class="column" @click="expand_cardPayment = true,expand_codPayment = false" :style="expand_cardPayment == true && expand_codPayment == false ? 'background-color: bisque;' :'' ">
+              <div class="flex flex-center">
+                <q-avatar>
+                  <img src="/img/icon/cardPayment.png" alt="">
+                </q-avatar>
+              </div>
+
+              <div class="q-mt-xs flex flex-center">
+                Chuyển khoản ngân hàng
+              </div>
+            </q-item>
+          </div>
+
+        </q-card-actions>
+        <q-separator />
+
+        <q-card-actions vertical>
+          <div v-if="expand_codPayment== true && expand_cardPayment == false">
+            <div>Ghi chú</div>
+            <div>
+              <q-input v-model="customer.paymentNotice" filled autogrow />
+            </div>
+          </div>
+          <div v-if="expand_cardPayment == true && expand_codPayment == false" >
+            <div class="column q-mt-lg">
+              <div>Tên Tài Khoản : Đào Văn Oanh</div>
+              <div>Số tài khoản: 198686868</div>
+              <div>Ngân hàng : MB BANK</div>
+              <div>Chi Nhánh: Gò Vấp</div>
+            </div>
+          </div>
+        </q-card-actions>
+        <q-card-actions >
+          <div class="row" style="width:100%">
+            <div class="col-6">
+              <q-btn label="Quay lại" color="negative"></q-btn>
+            </div>
+            <div class="col-6" v-if="expand_codPayment == true || expand_cardPayment == true">
+              <div style="display: flex; justify-content: flex-end;">
+                <q-btn  label="Đặt hàng" color="positive"></q-btn>
+              </div>
+            </div>
+          </div>
+        </q-card-actions>
+      </q-card>
+
+    <!-- Payment end -->
+
   </q-page>
 
 </template>
@@ -77,7 +153,7 @@ import { useQuasar } from "quasar";
 import mapActions from "vuex";
 import { DOMDirectiveTransforms } from "@vue/compiler-dom";
 const bill = ref({});
-const user = ref({});
+const customer = ref({});
 export default {
   name: "Cart",
   setup() {
@@ -107,82 +183,31 @@ export default {
     }
     return {
       product,
-      user,
+      customer,
       bill,
       items,
       hashmap,
+      dialog_payment: ref(true),
+      expand_codPayment: ref(false),
+      expand_cardPayment: ref(false),
       numberWithCommas,
       // input validation
       adresseRules: [
         (val) =>
           (val !== null && val !== "" && !!val) ||
-          "Please write your adresse!!",
+          "Xin vui lòng nhập địa chỉ của bạn!!",
       ],
       mobilRules: [
         (val) =>
           (val !== null && val !== "" && !!val) ||
-          "Please write a correct mobil",
+          "Xin vui lòng nhập đúng số điện thoại ạ",
         (val) =>
-          (val.includes(0) && !!val) || "A Mobil nummer must start with a  0",
+          (val.includes(0) && !!val) || "Xin vui lòng nhập SĐT bắt đầu bằng 0 ạ",
       ],
       //end Input validation
       dialog_bill: ref(false),
 
-      itemCheckOut() {
-        console.log("Check user : ", user.value);
-        if ($store.state.cache.cart.length === 0) {
-          router.replace("/product");
-          $q.notify({
-            message: "You have no items",
 
-            color: "negative",
-            avatar: `${WebApi.iconUrl}`,
-
-          }).catch((err) => {
-            console.log(err);
-          });
-          return;
-        }
-
-        // let product =[{}]
-        // for(Object a : $store.state.cache.cart ){
-        //   a.
-        // }
-
-        console.log("before Post request: ", $store.state.cache.cart);
-        axios({
-          method: "post",
-          url: `${WebApi.server}/checkOut`,
-          // data: JSON.stringify(product),
-
-          data: {
-            // name : $store.state.cache.cart.getName,
-            user: user.value,
-            product: $store.state.cache.cart,
-            total: $store.getters["cache/cartTotalPrice"],
-          },
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then(() => {
-            console.log("check Bill ", bill);
-
-            $q.notify({
-              message: "checkouted",
-
-              color: "positive",
-              avatar: `${WebApi.iconUrl}`,
-
-            });
-
-            router.replace("/thank");
-            $store.dispatch("cache/checkOut");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      },
     };
   },
   computed: {
@@ -194,6 +219,63 @@ export default {
   methods: {
     removeProductFromCart(product) {
       this.$store.dispatch("cache/removeProductFromCart", product);
+    },
+    itemCheckOut() {
+      this.dialog_bill = false
+      this.dialog_payment = true
+      console.log("Check user : ", customer.value);
+      if ($store.state.cache.cart.length === 0) {
+        router.replace("/product");
+        $q.notify({
+          message: "You have no items",
+
+          color: "negative",
+          avatar: `${WebApi.iconUrl}`,
+
+        }).catch((err) => {
+          console.log(err);
+        });
+        return;
+      }
+
+      // let product =[{}]
+      // for(Object a : $store.state.cache.cart ){
+      //   a.
+      // }
+
+      console.log("before Post request: ", $store.state.cache.cart);
+      axios({
+        method: "post",
+        url: `${WebApi.server}/checkOut`,
+        // data: JSON.stringify(product),
+
+        data: {
+          // name : $store.state.cache.cart.getName,
+          customer: customer.value,
+          product: $store.state.cache.cart,
+          total: $store.getters["cache/cartTotalPrice"],
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then(() => {
+          console.log("check Bill ", bill);
+
+          $q.notify({
+            message: "checkouted",
+
+            color: "positive",
+            avatar: `${WebApi.iconUrl}`,
+
+          });
+
+          router.replace("/thank");
+          $store.dispatch("cache/checkOut");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     // removeProductFromCart(product){
     //   this.$store.dispatch("cache/removeProductFromCart", product)
